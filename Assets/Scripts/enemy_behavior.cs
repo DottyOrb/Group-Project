dcef3443cd4a356404a779185b9f5988c0b4f58b
Anometrix <leszek.dopieralo1@gmail.com>
2025-03-15 23:20:43 +0000

    Merging Game Files
    Enemy Melee Ai
    Pathfinding

--- Assets/Scripts/enemy_behavior.cs
+++ Assets/Scripts/enemy_behavior.cs
@@ -5,17 +5,17 @@ using UnityEngine.AI;
 public class enemy_behavior : MonoBehaviour
 {
     #region Public Variables
-    public float speed;
+    public float speed; // Controls movement speed
     public float distanceBetween; // Distance for when Enemy stops moving if too close to player
-    public float attackCooldown; // Cooldown time between Attacks
-    public float attackPower; // How much damage the Enemy does
+    public float attackSpeed = 1f; // Cooldown time between Attacks
+    public int attackPower; // How much damage the Enemy does
+    public int health;
+    public int defence;
     #endregion
 
     #region Private Variables
     private float distance; // The distance between the Enemy and Player
-    private bool inRangeForAttack; // Determines if the Enemy is close enough to attack Player
-    private bool canAttack = true; // Controls Attack State
-    private Coroutine attackCoroutine;
+    private bool canAttack = true; // Controls Attack Delay
     NavMeshAgent agent;
     [SerializeField] Transform target;
     #endregion
@@ -29,8 +29,6 @@ public class enemy_behavior : MonoBehaviour
 
     void Update()
     {
-        //Debug.Log("Distance: "+distance+""+"InRangeForAttack: "+inRangeForAttack+""+"CanAttack+"+canAttack);
-
         distance = Vector2.Distance(transform.position, target.transform.position);
         Vector2 direction = target.transform.position - transform.position;
         direction.Normalize();
@@ -42,56 +40,45 @@ public class enemy_behavior : MonoBehaviour
             agent.SetDestination(target.position);
             agent.speed = speed;
             transform.rotation = Quaternion.Euler(Vector3.forward * angle);
-            inRangeForAttack = false;
-        }
-        else if (distance <= distanceBetween) // Checks whether the Enemy is close to the Player - Stops Movement, Enables Attack
-        {
-            inRangeForAttack = true;
         }
 
-        // Attack Conditions
-        if (inRangeForAttack && canAttack)
+        if (health <= 0)
         {
-            if (attackCoroutine == null) // Prevent multiple Coroutines from starting
-            {
-                canAttack = true;
-                attackCoroutine = StartCoroutine(AttackPlayer());
-            }
+            Destroy(gameObject);
         }
-        else if (!inRangeForAttack)
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
         {
-            if (attackCoroutine != null)
+            if (canAttack)
             {
-                StopCoroutine(attackCoroutine);
-                attackCoroutine = null;
+                StartCoroutine(AttackCooldown());
+                PlayerHealthPlaceholder.instance.DamagePlayer(attackPower);
             }
         }
     }
 
-    IEnumerator AttackPlayer()
+    IEnumerator AttackCooldown()
     {
         canAttack = false;
-        Debug.Log(("CoRoutine Started"));
 
-        // Input Player Damage Script
-        PlayerHealthPlaceholder.instance.DamagePlayer(5); // Player Damage Script
+        yield return new WaitForSeconds(attackSpeed);
 
-        yield return new WaitForSeconds(attackCooldown);
         canAttack = true;
-        attackCoroutine = null; // Reset the coroutine reference
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    public void DamageEnemy(int damage)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Placeholder"))
-        {
-            // Im thinking of checking if the weapon collides with the enemy not the player
-            // This is so that when the player walks into the enemy then the enemy wont get damaged unless the player attacks
-            // for defence, ill use this formula - Final Damage = Incoming Damage * (100/(100defence))
-        }
+        int finalDamage = damage * (100 / defence);
+        health -= finalDamage;
     }
 }
 
 // To Do List
 // Do Enemy Attack  - Damages Player with a cooldown
 // Make the Enemy be able to take Damage from the player
+// Formula for Defence Stat  is Final Damage = Incoming Damage * (100/(100defence))
+
