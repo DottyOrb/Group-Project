784bba883360b5376c93a97fc4c8b6f58623238f
Anometrix <leszek.dopieralo1@gmail.com>
2025-03-19 20:48:31 +0000

    Added Ranged Enemy Ai Added Enemy Bullets

---
+++ Assets/Scripts/Enemy_Bullet_Script.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Enemy_Bullet_Script : MonoBehaviour
+{
+    #region Public Variables
+    public float force;
+    #endregion
+
+    #region Private Variables
+    private Rigidbody2D rb;
+    [SerializeField] private float lifeTime = 3f;
+    #endregion
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = transform.right * force;
+    }
+}
