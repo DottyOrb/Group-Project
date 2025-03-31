a17f6640ad2082ddccbd425c6afa9dd044d0c37b
MilesIsACup <miles.jay2867@gmail.com>
2025-03-31 22:40:13 +0100

    Progress bar and health bar started

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+
+
+[ExecuteInEditMode()]
+public class HealthBar : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [MenuItem("GameObject/UI/Linear Progress Bar")]
+    public static void AddLinearProgressBar()
+    {
+        GameObject obj = Instantiate(Resources.Load<GameObject>("UI/Linear Progress Bar"));
+        obj.transform.SetParent(Selection.activeGameObject.transform, false);
+    }
+#endif
+
+    public int Minimum;
+    public int Maximum;
+    public int Current;
+    public Image Mask;
+    public Image Fill;
+    public Color Color;
+    public Player playerScript;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float currentOffset = Current - Minimum;
+        float maximumOffset = Maximum - Minimum;
+        float fillAmount = currentOffset / maximumOffset;
+        Mask.fillAmount = fillAmount;
+
+        Fill.color = Color;
+    }
+
+}
